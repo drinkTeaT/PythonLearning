1cb41f573033a9fea574f9c6419df73919f7bdea
Administrator <Administrator@192.168.1.109>
2018-02-22 23:30:43 +0800

    列表推导  按条件循环一个列表中所有值  ;如何在列表推导内过滤列表中字符串？

--- Learning/list/ListDemo.py
+++ Learning/list/ListDemo.py
@@ -1,24 +1,10 @@
 #coding:gbk
 '''
-for循环  ;if else;只打印 person 列表中的数字
+列表推导  按条件循环一个列表中所有值  ;如何在列表推导内过滤列表中字符串？
 '''
 
-person = ["male","female","kid",23,1.4,"female","tom","jack",34,998,"汉字",943242423.23232323232]
-number  = 12
-'''i是一个变量，用来取列表中的值'''
-for i in person:
-    print(i)
+data1 = [1,90,33,74,3.14,99,5.432,102.3]
 
-'''等于号是  == '''
-if isinstance(number, list) == True:
-    print("是真的！")
-else:
-    print("是假的！")
-
-print("整数 的数据类型",type(person[3])," 小数的数据类型 ",type(person[4]),"字符串数据类型",type(person[0]))
-
-'''打印 person 列表中的数字'''
-for i in person:
-    if isinstance(i, int) == True or isinstance(i, float) == True : 
-        '''如果i是小数 或 整数就显示该值 '''
-        print(i)
+'''把data1数字中大于50的放入data2列表内'''
+data2 = [i for i in data1  if i > 50 ]
+print(data2)

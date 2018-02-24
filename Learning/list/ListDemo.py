6f4908763397bdf1696c184c888f67614126693e
Administrator <Administrator@192.168.1.109>
2018-02-24 23:04:20 +0800

    函数介绍

--- Learning/list/ListDemo.py
+++ Learning/list/ListDemo.py
@@ -1,10 +1,33 @@
 #coding:gbk
 '''
-列表推导  按条件循环一个列表中所有值  ;如何在列表推导内过滤列表中字符串？
+函数部分
 '''
 
-data1 = [1,90,33,74,3.14,99,5.432,102.3]
+'''创建函数'''
+def test ():
+    print("hello world")
+    
+'''创建函数'''
+''' 有return该函数返回一个值'''
+def test1(value):
+    '''定义一个变量 sum用来对列表数据求和'''
+    sum = 1
+    '''对列表数据进行遍历（把里面的值一个个拿出来）'''
+    for i in value:
+        sum = sum * i
+    return sum
 
-'''把data1数字中大于50的放入data2列表内'''
-data2 = [i for i in data1  if i > 50 ]
-print(data2)
+value = [1,2,3,2,2,3,1]
+print(test1(value))
+
+
+
+'''变量    重复赋值'''
+abc = 45
+abc = 54
+fgh = [1,4,"t8iy"]
+
+''' 常量  不能赋值'''
+12
+"asdfasf"
+4.42

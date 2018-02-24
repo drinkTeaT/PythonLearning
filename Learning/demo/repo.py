c91c1979a42f1c6c42eac7b21e3f531995363490
Administrator <Administrator@192.168.1.109>
2018-02-24 23:53:55 +0800

    引用其他模块的函数 以及缺省函数的作用

---
+++ Learning/demo/repo.py
@@ -0,0 +1,11 @@
+'''
+Created on 2018年2月24日
+
+@author: Administrator
+'''
+def xwo(a,b,c,d,f,g,h = 8,flag = True):
+    if flag == True:
+        sum = a+b+c+d+f+g
+    else:
+        sum = a+h
+    return sum
--- Learning/list/ListDemo.py
+++ Learning/list/ListDemo.py
@@ -1,18 +1,7 @@
 #coding:gbk
 '''
-函数部分 缺省赋值 以及缺省函数的作用
+引用其他模块的函数 以及缺省函数的作用
 '''
-
-
-'''
-def 函数名（参数）:
-         代码组 
-创建缺省函数'''
-def abc(a,b = 3):
-    return a+b
-
-n = abc(6,2)
-print(n)
-
-n = abc(1,2)
+from demo import repo
+n = repo.xwo(1,2,3,4,5,6)
 print(n)

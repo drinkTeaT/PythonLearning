c699c39236e6438f5bba59f051e0a420e77e31eb
Administrator <Administrator@192.168.1.109>
2018-02-24 23:23:50 +0800

    函数部分  计算两个列表数据之和

--- Learning/list/ListDemo.py
+++ Learning/list/ListDemo.py
@@ -1,33 +1,25 @@
 #coding:gbk
 '''
-函数部分
+函数部分  计算两个列表数据之和
 '''
 
 '''创建函数'''
 def test ():
     print("hello world")
     
-'''创建函数'''
-''' 有return该函数返回一个值'''
-def test1(value):
-    '''定义一个变量 sum用来对列表数据求和'''
-    sum = 1
-    '''对列表数据进行遍历（把里面的值一个个拿出来）'''
-    for i in value:
-        sum = sum * i
-    return sum
+list1 = [12,23,8,9]
+list2 = [6,2,3]
 
-value = [1,2,3,2,2,3,1]
-print(test1(value))
+def test1(a,b):
+    sum1 = 0
+    for i in a:
+        sum1 = sum1 + i
+    
+    sum2 = 0
+    for i in b:
+        sum2 = sum2 + i
+    
+    return sum1+sum2
 
-
-
-'''变量    重复赋值'''
-abc = 45
-abc = 54
-fgh = [1,4,"t8iy"]
-
-''' 常量  不能赋值'''
-12
-"asdfasf"
-4.42
+n = test1(list1, list2)
+print(n)

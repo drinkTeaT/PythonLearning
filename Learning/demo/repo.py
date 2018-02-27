f68d12d22849b39c63cd1d74774a48f5bb1673e0
Administrator <Administrator@192.168.1.109>
2018-02-27 22:17:31 +0800

    有无返回值函数，以及计算平均值

--- Learning/demo/repo.py
+++ Learning/demo/repo.py
@@ -1,17 +1,77 @@
 '''
 
-文件的读和写    for循环里面的readline只打印某一行？
-
+基础知识  变量，常量，数据类型，函数返回值
+变量是可以被重复赋值的，而且是名字是自定义的
+常量不准被赋值
+数据类型 常见的有 整数  int,小数 float ，列表 list ，字符串 str，字典 dict，集合 set等等  检测数据类型 用 type(参数)
 '''
-import os
-'''打开文件所在目录'''
-os.chdir("D:\\")
-''' 找到具体 “文件名.后缀 ”'''
-data = open("ss.txt","a")
+a = 45
+a = 8979.36
+b = "var"
 
-''' 删除原有数据写入新数据'''
-print("/\nss",file = data)
-''' 增加新数据'''
-data.write("胜多负少单方事故")
-'''关闭文件'''
-data.close()
+c = []
+
+d = {}
+d = dict()
+
+e = set()
+
+type(e)
+
+''' 函数返回值'''
+#定义一个  无参，没有返回值的函数
+def cetaphil():
+    for i in c:
+        print(i)
+        
+#调用 函数
+cetaphil()
+
+#定义一个 有参，有返回值的函数 
+#传递两个数据到该函数，并返回一个包含这两个数据的列表数据类型
+def pen(b,aa=1):
+    c = [1,2,3]    
+    c.append(b)
+    c.append(aa)
+    return c
+#调用函数
+pen(1,2)
+
+''' 给定一个列表，里面包含小数，整数。得出他的平均数 用有返回值函数实现'''
+data = [1,6,1.34,2,3,4,5.14]
+def avg(a):
+    d = 0
+    for i in a:
+        d = d + i
+    f = d/len(data)
+    return f
+
+print(avg(data))   # a = data
+
+''' 给定一个列表，里面包含小数，整数。在函数内部打印出他的平均数 用无返回值函数实现'''
+
+def soft(a):
+    b = 0
+    for i in a:
+        b = b + i
+    h = b/len(a)
+    print(h)
+
+data = [1,6,1.34,2,3,4,5.14]
+soft(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

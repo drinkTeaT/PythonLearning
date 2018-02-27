d86366acaa579d7e8ec840bd12f6ac628b68bbd0
Administrator <Administrator@192.168.1.109>
2018-02-27 20:20:02 +0800

    字典 键值对

--- Learning/demo/repo.py
+++ Learning/demo/repo.py
@@ -1,11 +1,16 @@
 '''
-Created on 2018年2月24日
 
-@author: Administrator
+字典  键-值对
+
 '''
-def xwo(a,b,c,d,f,g,flag = True,h = 8):
-    if flag == True:
-        sum = a+b+c+d+f+g
-    else:
-        sum = a+h
-    return sum
+#创建一个字典
+data1 = {}
+data2 = dict()
+
+#往字典里设置 “键值对”
+data1["MyName"] = "Jack"
+data1["Gerder"] = "male"
+data1["age"]    = 18
+data1["hobby"] = ["football","baseball","drawing"]
+
+print(data1)
--- Learning/list/ListDemo.py
+++
@@ -1,7 +0,0 @@
-#coding:gbk
-'''
-引用其他模块的函数 以及缺省函数的作用
-'''
-from demo import repo
-n = repo.xwo(1,2,3,4,5,6)
-print(n)
